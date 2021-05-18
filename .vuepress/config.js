44e986098d7695a9204fa22a4ab54e70d0998ad1
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-05-18 16:47:53 +0900

    Delete config.js

--- .vuepress/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-module.exports = {
-    title: 'monun-docs', // 사이트 타이틀
-    themeConfig: {
-      logo: 'https://avatars.githubusercontent.com/u/77531416?v=4', // 로고 이미
-      sidebar: 'auto' // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
-    }
-  }
-  
