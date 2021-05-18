099242f86501ed72ccc9778f81b7fe18834dd972
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-05-18 16:14:55 +0900

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,10 +3,7 @@ module.exports = {
     title: 'monun-docs', // 사이트 타이틀
     description: 'Documentation contribute purposes. For Monun.',
     themeConfig: {
-      logo: 'https://avatars.githubusercontent.com/u/77531416?v=4', // 로고 이미지
-      nav: [
-        { text: 'Home', link: '/' }
-      ],
+      logo: 'https://avatars.githubusercontent.com/u/77531416?v=4', // 로고 이미
       sidebar: 'auto' // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
     }
   }
