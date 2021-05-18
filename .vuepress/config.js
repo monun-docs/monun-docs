0db05a51b7defad579158485fe76fae45dd5a2be
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-05-18 16:41:39 +0900

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,25 +1,6 @@
 // config.js
 module.exports = {
     title: 'monun-docs', // 사이트 타이틀
-    description: 'Documentation contribute purposes. For Monun.',
-    head: [
-        ['meta', {
-            name: 'theme-color',
-            content: '#3eaf7c'
-        }],
-        ['meta', {
-            name: 'apple-mobile-web-app-capable',
-            content: 'yes'
-        }],
-        ['meta', {
-            name: 'apple-mobile-web-app-status-bar-style',
-            content: 'black'
-        }],
-        ["link", {
-            rel: "'stylesheet",
-            href: "/styles/website.css"
-        },]
-    ],
     themeConfig: {
       logo: 'https://avatars.githubusercontent.com/u/77531416?v=4', // 로고 이미
       sidebar: 'auto' // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
