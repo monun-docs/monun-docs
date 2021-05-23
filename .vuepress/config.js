56f2cd7d92efa6946299af338b83d694c7afc1ca
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-05-23 23:04:22 +0900

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,8 +3,9 @@ module.exports = {
     title: 'monun-docs', // 사이트 타이틀
     description: 'Documentation contribute purposes. For Monun.',
     themeConfig: {
-      logo: 'https://avatars.githubusercontent.com/u/77531416?v=4', // 로고 이미
+      logo: 'https://avatars.githubusercontent.com/u/77531416?v=4', // monun logo
       sidebar: 'auto' // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
+      // 사이드바 설정 귀찮다
     }
   }
   
