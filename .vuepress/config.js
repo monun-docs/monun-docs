865a81c56cecee05ec0596b484619e5b115900fb
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-05-23 23:16:35 +0900

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,8 +4,19 @@ module.exports = {
     description: 'Documentation contribute purposes. For Monun.',
     themeConfig: {
       logo: 'https://avatars.githubusercontent.com/u/77531416?v=4', // monun logo
-      sidebar: 'auto' // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
-      // 사이드바 설정 귀찮다
+      sidebar: [
+          ['/', 'Intro'],
+          ['/farm', 'Farm'],
+          ['/inv-captive', 'Inv-Captive'],
+          ['/invfx', 'InvFX'],
+          ['/kommand', 'Kommand'],
+          ['/kotlin-plugin', 'Kotlin-Plugin'],
+          ['/minigame-fortress', 'Minigame-Fortress'],
+          ['/parkour-maker', 'Parkour-Maker'],
+          ['/physics', 'Physics'],
+          ['/regions', 'Regions'],
+          ['/tap', 'Tap'],
+          
     }
   }
   
