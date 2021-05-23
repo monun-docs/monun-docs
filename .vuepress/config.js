7fccc49c460cc4b6ed8b5c1491810524bbf5b83d
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-05-23 23:19:50 +0900

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,19 +4,7 @@ module.exports = {
     description: 'Documentation contribute purposes. For Monun.',
     themeConfig: {
       logo: 'https://avatars.githubusercontent.com/u/77531416?v=4', // monun logo
-      sidebar: [
-          ['/', 'Intro'],
-          ['/farm', 'Farm'],
-          ['/inv-captive', 'Inv-Captive'],
-          ['/invfx', 'InvFX'],
-          ['/kommand', 'Kommand'],
-          ['/kotlin-plugin', 'Kotlin-Plugin'],
-          ['/minigame-fortress', 'Minigame-Fortress'],
-          ['/parkour-maker', 'Parkour-Maker'],
-          ['/physics', 'Physics'],
-          ['/regions', 'Regions'],
-          ['/tap', 'Tap'],
-          ]
+      sidebar: 'auto'
     }
   }
   
