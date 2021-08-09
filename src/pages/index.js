acbacacff01ad5d72ac390139cb7dc9f971e4f12
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-08-09 16:11:38 +0900

    change name

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,8 +37,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Main Page`}
+      description="monun-docs main page">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
