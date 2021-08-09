63d47f3d25e95f27c81f61f5e8d36e1e3a8735e7
issac4892 <56223465+issac4892@users.noreply.github.com>
2021-08-09 16:15:36 +0900

    fix edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/hyeonworks/monun-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
