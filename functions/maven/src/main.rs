435318f8b5c67ceff875564ea63d42445d268f57
dolphin2410 <dolphin2410@outlook.kr>
2022-03-20 01:59:51 +0900

    Functions CORS bypass

---
+++ functions/maven/src/main.rs
@@ -0,0 +1,56 @@
+use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use aws_lambda_events::encodings::Body;
+use http::header::HeaderMap;
+use hyper::{Client, body};
+use hyper_tls::HttpsConnector;
+use lambda_runtime::{handler_fn, Context, Error};
+use log::LevelFilter;
+use serde::{Serialize, Deserialize};
+use simple_logger::SimpleLogger;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Response {
+    versioning: Versioning
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Versioning {
+    latest: String
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
+
+    let func = handler_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, Error> {
+    let https = HttpsConnector::new();
+
+    let client = Client::builder().build::<_, hyper::Body>(https);
+
+    let format = format!("https://repo.maven.apache.org/maven2/{}/{}/maven-metadata.xml", event.query_string_parameters.get("group").unwrap().replace(".", "/"), event.query_string_parameters.get("artifact").unwrap());
+
+    let uri = format.parse()?;
+
+    let response = client.get(uri).await?;
+
+    let body_bytes = body::to_bytes(response.into_body()).await?;
+
+    let str = String::from_utf8(body_bytes.to_vec())?;
+
+    let res = serde_xml_rs::from_str::<Response>(str.as_str())?;
+
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: HeaderMap::new(),
+        multi_value_headers: HeaderMap::new(),
+        body: Some(Body::Text(format!("{}", res.versioning.latest))),
+        is_base64_encoded: Some(false),
+    };
+
+    Ok(resp)
+}
--- src/components/MavenVersion.js
+++ src/components/MavenVersion.js
@@ -2,17 +2,20 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react';
 import CodeBlock from '@theme/CodeBlock';
 
-const fetchLatestVersion = (artifact, callback) => {
-    axios.get(`https://img.shields.io/maven-central/v/${artifact.replace(":", "/")}`)
+const fetchLatestVersion = (notation, callback) => {
+    let split = notation.split(":")
+    let group = split[0]
+    let artifact = split[1]
+    axios.get(`/maven/?group=${group}&artifact=${artifact}`)
         .then(response => {
-            callback(response.data.match(/(?<="maven-central: v)(.*?)(?=")/g))
+            callback(response.data)
         })
 }
 
-const MavenVersion = ({children, artifact, language}) => {
+const MavenVersion = ({children, notation, language}) => {
     const [result, setResult] = useState("");
 
-    useEffect(() => fetchLatestVersion(artifact, (data) => setResult(data)), [])
+    useEffect(() => fetchLatestVersion(notation, (data) => setResult(data)), [])
 
     return (
         <CodeBlock language={language}>
