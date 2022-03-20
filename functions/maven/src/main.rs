a7ec6aa478430f9c100d32f467acbe05edd3f417
dolphin2410 <dolphin2410@outlook.kr>
2022-03-20 12:42:07 +0900

    resolve bad openssl

--- functions/maven/src/main.rs
+++ functions/maven/src/main.rs
@@ -1,8 +1,7 @@
 use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
 use aws_lambda_events::encodings::Body;
+use curl::easy::Easy;
 use http::header::HeaderMap;
-use hyper::{Client, body};
-use hyper_tls::HttpsConnector;
 use lambda_runtime::{handler_fn, Context, Error};
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
@@ -17,19 +16,20 @@ async fn main() -> Result<(), Error> {
 }
 
 pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, Error> {
-    let https = HttpsConnector::new();
-
-    let client = Client::builder().build::<_, hyper::Body>(https);
-
     let format = format!("https://repo.maven.apache.org/maven2/{}/{}/maven-metadata.xml", event.query_string_parameters.get("group").unwrap().replace(".", "/"), event.query_string_parameters.get("artifact").unwrap());
 
-    let uri = format.parse()?;
+    let mut body_bytes = String::new();
 
-    let response = client.get(uri).await?;
-
-    let body_bytes = body::to_bytes(response.into_body()).await?;
-
-    let str = String::from_utf8(body_bytes.to_vec())?;
+    let mut easy = Easy::new();
+    easy.url(format.as_str()).unwrap();
+    {
+        let mut transfer = easy.transfer();
+        transfer.write_function(|data| {
+            body_bytes.push_str(String::from_utf8(data.to_vec()).unwrap().as_str());
+            Ok(data.len())
+        }).unwrap();
+        transfer.perform().unwrap();
+    }
 
     let regex = fancy_regex::Regex::new(r#"(?<=<latest>)(.*?)(?=<\/latest>)"#)?;
 
@@ -37,7 +37,7 @@ pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> 
         status_code: 200,
         headers: HeaderMap::new(),
         multi_value_headers: HeaderMap::new(),
-        body: Some(Body::Text(format!("{}", regex.find(str.as_str()).unwrap().unwrap().as_str()))),
+        body: Some(Body::Text(format!("{}", regex.find(body_bytes.as_str()).unwrap().unwrap().as_str()))),
         is_base64_encoded: Some(false),
     };
 
