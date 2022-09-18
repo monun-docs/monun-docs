e6e8dc12ed0ec2b2ca860ebdeecc2cde313391f6
dolphin2410 <dolphin2410@outlook.kr>
2022-09-18 15:47:31 +0900

    swizzle

---
+++ src/theme/CodeBlock/Line/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+export default function CodeBlockLine({
+  line,
+  classNames,
+  showLineNumbers,
+  getLineProps,
+  getTokenProps,
+  onClick
+}) {
+  if (line.length === 1 && line[0].content === '\n') {
+    line[0].content = '';
+  }
+  const lineProps = getLineProps({
+    line,
+    onClick,
+    className: clsx(classNames, showLineNumbers && styles.codeLine),
+  });
+  const lineTokens = line.map((token, key) => (
+    <span key={key} {...getTokenProps({token, key})} />
+  ));
+  return (
+    <span {...lineProps}>
+      {showLineNumbers ? (
+        <>
+          <span className={styles.codeLineNumber} />
+          <span className={styles.codeLineContent}>{lineTokens}</span>
+        </>
+      ) : (
+        <>
+          {lineTokens}
+          <br />
+        </>
+      )}
+    </span>
+  );
+}
--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -4,159 +4,135 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React, {isValidElement, useEffect, useState} from 'react';
-import clsx from 'clsx';
-import Highlight, {defaultProps} from 'prism-react-renderer';
-import {
-  useThemeConfig,
-  parseCodeBlockTitle,
-  parseLanguage,
-  parseLines,
-  ThemeClassNames,
-  usePrismTheme,
-} from '@docusaurus/theme-common';
-import CopyButton from '@theme/CodeBlock/CopyButton';
-import styles from './styles.module.css';
-import tap from '@site/static/links/tap-links.json';
-export default function CodeBlock({
-  children,
-  className: blockClassName = '',
-  metastring,
-  title,
-  language: languageProp,
-}) {
-  const {prism} = useThemeConfig();
-  const [mounted, setMounted] = useState(false); // The Prism theme on SSR is always the default theme but the site theme
-  // can be in a different mode. React hydration doesn't update DOM styles
-  // that come from SSR. Hence force a re-render after mounting to apply the
-  // current relevant styles. There will be a flash seen of the original
-  // styles seen using this current approach but that's probably ok. Fixing
-  // the flash will require changing the theming approach and is not worth it
-  // at this point.
 
-  useEffect(() => {
-    setMounted(true);
-  }, []); // We still parse the metastring in case we want to support more syntax in the
-  // future. Note that MDX doesn't strip quotes when parsing metastring:
-  // "title=\"xyz\"" => title: "\"xyz\""
-
-  const codeBlockTitle = parseCodeBlockTitle(metastring) || title;
-  const prismTheme = usePrismTheme(); // <pre> tags in markdown map to CodeBlocks and they may contain JSX children.
-  // When the children is not a simple string, we just return a styled block
-  // without actually highlighting.
-
-  if (React.Children.toArray(children).some((el) => isValidElement(el))) {
-    return (
-      <Highlight
-        {...defaultProps}
-        key={String(mounted)}
-        theme={prismTheme}
-        code=""
-        language={'text'}>
-        {({className, style}) => (
-          <pre
-            /* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */
-            tabIndex={0}
-            className={clsx(
-              className,
-              styles.codeBlockStandalone,
-              'thin-scrollbar',
-              styles.codeBlockContainer,
-              blockClassName,
-              ThemeClassNames.common.codeBlock,
-            )}
-            style={style}>
-            <code className={styles.codeBlockLines}>{children}</code>
-          </pre>
-        )}
-      </Highlight>
-    );
-  } // The children is now guaranteed to be one/more plain strings
-
-  const content = Array.isArray(children) ? children.join('') : children;
-  const language =
-    languageProp ?? parseLanguage(blockClassName) ?? prism.defaultLanguage;
-  const {highlightLines, code} = parseLines(content, metastring, language);
-  return (
-    <Highlight
-      {...defaultProps}
-      key={String(mounted)}
-      theme={prismTheme}
-      code={code}
-      language={language ?? 'text'}>
-      {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <div
-          className={clsx(
-            styles.codeBlockContainer,
-            blockClassName,
-            {
-              [`language-${language}`]:
-                language && !blockClassName.includes(`language-${language}`),
-            },
-            ThemeClassNames.common.codeBlock,
-          )}>
-          {codeBlockTitle && (
-            <div style={style} className={styles.codeBlockTitle}>
-              {codeBlockTitle}
-            </div>
-          )}
-          <div className={styles.codeBlockContent} style={style}>
-            <pre
-              /* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */
-              tabIndex={0}
-              className={clsx(className, styles.codeBlock, 'thin-scrollbar')}>
-              <code className={styles.codeBlockLines}>
-                {(() => {
-                  let link = ""
-                  return tokens.map((line, i) => {
-                    if (line.length === 1 && line[0].content === '\n') {
-                      line[0].content = '';
-                    }
-
-                    if (i == 0) {
-                      link = line[0].content.replace(/\s/g, '').replace(/^\/\//g, "")
-                    }
+ import React from 'react';
+ import clsx from 'clsx';
+ import {useThemeConfig, usePrismTheme} from '@docusaurus/theme-common';
+ import {
+   parseCodeBlockTitle,
+   parseLanguage,
+   parseLines,
+   containsLineNumbers,
+   useCodeWordWrap,
+ } from '@docusaurus/theme-common/internal';
+ import tap from '@site/static/links/tap-links.json'
+ import Highlight, { defaultProps } from 'prism-react-renderer';
+ import Line from '@theme/CodeBlock/Line';
+ import CopyButton from '@theme/CodeBlock/CopyButton';
+ import WordWrapButton from '@theme/CodeBlock/WordWrapButton';
+ import Container from '@theme/CodeBlock/Container';
+ 
+ import styles from './styles.module.css';
+ 
+ export default function CodeBlock({
+   children,
+   className: blockClassName = '',
+   metastring,
+   title: titleProp,
+   showLineNumbers: showLineNumbersProp,
+   language: languageProp,
+ }) {
+   const {
+     prism: {defaultLanguage, magicComments},
+   } = useThemeConfig();
+   const language =
+     languageProp ?? parseLanguage(blockClassName) ?? defaultLanguage;
+   const prismTheme = usePrismTheme();
+   const wordWrap = useCodeWordWrap();
+ 
+   // We still parse the metastring in case we want to support more syntax in the
+   // future. Note that MDX doesn't strip quotes when parsing metastring:
+   // "title=\"xyz\"" => title: "\"xyz\""
+   const title = parseCodeBlockTitle(metastring) || titleProp;
+ 
+   const {lineClassNames, code} = parseLines(children, {
+     metastring,
+     language,
+     magicComments,
+   });
+   const showLineNumbers =
+     showLineNumbersProp ?? containsLineNumbers(metastring);
+ 
+   return (
+     <Container
+       as="div"
+       className={clsx(
+         blockClassName,
+         language &&
+           !blockClassName.includes(`language-${language}`) &&
+           `language-${language}`,
+       )}>
+       {title && <div className={styles.codeBlockTitle}>{title}</div>}
+       <div className={styles.codeBlockContent}>
+         <Highlight
+           {...defaultProps}
+           theme={prismTheme}
+           code={code}
+           language={(language ?? 'text')}>
+           {({className, tokens, getLineProps, getTokenProps}) => (
+             <pre
+               /* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */
+               tabIndex={0}
+               ref={wordWrap.codeBlockRef}
+               className={clsx(className, styles.codeBlock, 'thin-scrollbar')}>
+               <code
+                 className={clsx(
+                   styles.codeBlockLines,
+                   showLineNumbers && styles.codeBlockLinesWithNumbering,
+                 )}>
+                  {(() => {
+                    let link = ""
+                    return tokens.map((line, i) => {
+                      if (line.length === 1 && line[0].content === '\n') {
+                        line[0].content = '';
+                      }
   
-                    const lineProps = getLineProps({
-                      line,
-                      key: i,
-                    });
-  
-                    if (highlightLines.includes(i)) {
-                      lineProps.className += ' docusaurus-highlight-code-line';
-                    }
-  
-                    return (
-                      <span key={i} {...lineProps}
-                      onClick={() => {
-                        if (language == "kodef") {
-                          window.location.href = tap[link]
-                        }
-                      }}>
-                        {line.map((token, key) => (
-                          <span
-                            key={key}
-                            {...getTokenProps({
-                              token,
-                              key,
-                            })}
-                          />
-                        ))}
-                        <br />
-                      </span>
-                    );
-                  })
-                })()}
-              </code>
-            </pre>
-            {(() => {
+                      if (i == 0) {
+                        link = line[0].content.replace(/\s/g, '').replace(/^\/\//g, "")
+                      }
+    
+                      const lineProps = getLineProps({
+                        line,
+                        key: i,
+                      });
+    
+                      if (lineClassNames[i]) {
+                        lineProps.className += ' docusaurus-highlight-code-line';
+                      }
+                      return (<Line
+                        key={i}
+                        line={line}
+                        getLineProps={getLineProps}
+                        getTokenProps={getTokenProps}
+                        classNames={lineClassNames[i]}
+                        showLineNumbers={showLineNumbers}
+                        onClick={() => {
+                          if (language == "kodef") {
+                            window.location.href = tap[link]
+                          }
+                        }}
+                      />)
+                    })
+                  })()}
+               </code>
+             </pre>
+           )}
+         </Highlight>
+         <div className={styles.buttonGroup}>
+           {(wordWrap.isEnabled || wordWrap.isCodeScrollable) && (
+             <WordWrapButton
+               className={styles.codeButton}
+               onClick={() => wordWrap.toggle()}
+               isEnabled={wordWrap.isEnabled}
+             />
+           )}
+                       {(() => {
                 if (language != "kodef") {
                   return <CopyButton code={code} />
                 }
               })()}
-          </div>
-        </div>
-      )}
-    </Highlight>
-  );
-}
+         </div>
+       </div>
+     </Container>
+   );
+ }
