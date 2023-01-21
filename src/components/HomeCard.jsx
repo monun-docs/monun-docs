c7b070ea5e1c291ba356e889ea7ff3f2ab828944
dolphin2410 <dolphin2410@outlook.kr>
2023-01-21 11:04:06 +0900

    fix url

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -6,9 +6,9 @@ import DocsCard from './DocsCard';
 export default function HomeCard() {
     return (
         <CardContainer num={2} className={styles.card_container}>
-            <DocsCard title={"라이브러리"} desc={"개발자들을 위한 라이브러리 사용법"} url={"libraries"} />
-            <DocsCard title={"개발"} desc={"자바 설치 등 개발 환경 설정법"} url={"usage"} />
-            <DocsCard title={"플러그인"} desc={"각별님의 플러그인 사용법"} url={"plugins"} />
+            <DocsCard title={"라이브러리"} desc={"개발자들을 위한 라이브러리 사용법"} url={"libraries/"} />
+            <DocsCard title={"개발"} desc={"자바 설치 등 개발 환경 설정법"} url={"usage/"} />
+            <DocsCard title={"플러그인"} desc={"각별님의 플러그인 사용법"} url={"plugins/"} />
         </CardContainer>
     );
 }
